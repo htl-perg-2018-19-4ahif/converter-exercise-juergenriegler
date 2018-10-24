5b5aa8023cc25bfd04c76f9d8ce7065812787f59
Juergen Riegler <juergen.riegler16@gmail.com>
2018-10-24 17:13:33 +0200

    Finished with 7 lines of Code

--- convert.js
+++ convert.js
@@ -1,1 +1,7 @@
-console.log('Not implemented yet!');
+let args = process.argv.slice(2);
+if (args.length !== 4 || args[2] !== 'to' || isNaN(args[0])) err();
+try {
+    let r = require('convert-units')(args[0]).from(args[1]).to(args[3]);
+    console.log(`${args[0]} ${args[1]} are ${r} ${args[3]}`);
+} catch (ex) { err(); }
+function err() { console.log("Invalid parameters"); process.exit(-1); }
